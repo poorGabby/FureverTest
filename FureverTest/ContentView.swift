b32ec520a3a9a14b0d88787f9af3827859ad00a2
Gabby <gabbygg129@gmail.com>
2026-01-28 11:54:10 +0800

    Initial Commit

---
+++ FureverTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FureverTest
+//
+//  Created by Gabby on 2026/1/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FureverTest/FureverTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FureverTestApp.swift
+//  FureverTest
+//
+//  Created by Gabby on 2026/1/28.
+//
+
+import SwiftUI
+
+@main
+struct FureverTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
